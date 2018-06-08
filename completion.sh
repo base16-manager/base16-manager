df28df6af5e61326a8af5496c198b1459e00a1c4
theova <theova@users.noreply.github.com>
2018-06-08 20:51:31 +0200

    Add bash completion
    Works the same as zsh.

--- completion.sh
+++ completion.sh
@@ -2,51 +2,65 @@
 
 _base16-manager_methods() {
 # print out all possible methods, e.g set-random
-base16-manager | sed 1,2d | cut -s -d ' ' -f 5
+base16-manager | sed 1,2d | cut -s -d ' ' -f 2
 }
 
-_base16-manager_list-installable() {
-local RESULT=""
-for REPO in $(base16-manager list-support); do
-	if ! grep -q "$REPO" <<< $(base16-manager list)  ; then
-		if [ -z $RESULT ]; then
-			RESULT="$REPO"
-		else
-			RESULT="$RESULT\\n$REPO"
-		fi
-	fi
-done 
-echo "$RESULT"
-}
 
+_base16-manager_bash() {
+local cur prev opts
+COMPREPLY=()
+cur="${COMP_WORDS[COMP_CWORD]}"
+prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+# first: method
+if [[ -n ${prev} ]]; then
+  opts=$(_base16-manager_methods)
+fi
+
+# second: arguments
+case ${prev} in
+  "set")
+    opts=$(base16-manager list-themes)
+    ;;
+  "install")
+    opts=$(base16-manager list-installable)
+    ;;
+  "uninstall")
+    opts=$(base16-manager list)
+    ;;
+esac
+
+# generate completion
+COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+}
 
 _base16-manager_zsh() {
 _arguments '1: :->method' '2: :->argument'
 case $state in
-	method)
-		compadd $(_base16-manager_methods)
-		;;
-	argument)
-
-		local METHOD=${(Q)words[CURRENT-1]}
-
-		if [ "$METHOD" = "set" ]; then
-			compadd $(base16-manager list-themes)
-		elif [ "$METHOD" = "install" ]; then
-			compadd $(_base16-manager_list-installable)
-		elif [ "$METHOD" = "uninstall" ]; then
-			compadd  $(base16-manager list)
-		fi
-		;;
+  method)
+    compadd $(_base16-manager_methods)
+    ;;
+  argument)
+    local METHOD=${(Q)words[CURRENT-1]}
+    if [ "$METHOD" = "set" ]; then
+      compadd $(base16-manager list-themes)
+    elif [ "$METHOD" = "install" ]; then
+      compadd $(base16-manager list-installable)
+    elif [ "$METHOD" = "uninstall" ]; then
+      compadd $(base16-manager list)
+    fi
+    ;;
 esac 
 }
 
+# MAIN
 if type complete >/dev/null 2>/dev/null; then
-	# bash
-  # TODO
-elif type compdef >/dev/null 2>/dev/null; then
-	# zsh
-	compdef _base16-manager_zsh base16-manager
+  # bash
+  complete -F _base16-manager_bash base16-manager
 fi
+if type compdef >/dev/null 2>/dev/null; then
+  # zsh
+  compdef _base16-manager_zsh base16-manager
+fi; 
 
 # vim: ts=2 sw=2 et
